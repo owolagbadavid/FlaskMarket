c5d37947ed8983692d3262ef3632fc7e9d3ee912
DAVID <owolagbadavid@gmail.com>
2024-03-18 17:04:54 +0100

    validate

--- market/routes.py
+++ market/routes.py
@@ -1,6 +1,6 @@
-from market import app
-from flask import render_template
-from market.models import Item
+from market import app, db
+from flask import render_template, redirect, url_for, flash
+from market.models import Item, User
 from market.forms import RegisterForm
 
 
@@ -16,9 +16,16 @@ def market_page():
     return render_template('market.html', items=items)
 
 
-@app.route('/register')
+@app.route('/register', methods=['GET', 'POST'])
 def register_page():
     form = RegisterForm()
     if form.validate_on_submit():
-        pass
+        user_to_create = User(username=form.username.data,
+                              email_address=form.email_address.data, password_hash=form.password1.data)
+        db.session.add(user_to_create)
+        db.session.commit()
+        return redirect(url_for('market_page'))
+    if form.errors != {}:
+        for err_msg in form.errors.values():
+            flash(f'Error {err_msg}', "danger")
     return render_template('register.html', form=form)
